70a31465e85989411c5c7abb399ad8f5fa03212c
Nelson Ramirez <xhyskoax@gmail.com>
2023-03-21 21:06:47 -0300

    crud completo de usuarios

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import { getUsuarios, getUsuariosById, addUsuario, updateUsuario, deleteUsuariosById} from './database/consultas.js' 
+
+
 const app = express();
+
 
 //MIDDLEWARE
 app.use(express.json());
@@ -8,7 +12,62 @@ app.use(express.urlencoded({extended:false}));
 app.listen(3000, () => console.log("http://localhost:3000/"))
 
 
-//rutas
+//rutas GET
 app.get("/usuarios", (req, res) => {
-    res.status(200).json({code: 200, message: "Enviando datos de usuario."})
-} )
+    getUsuarios().then(usuarios => {
+        res.status(200).json({code: 200, data: usuarios})
+    }).catch(error => {
+        console.log(error)
+        res.status(500).json({code: 500, error: "Error al cargar los usuarios."})
+    })
+})
+
+//rutas GET POR ID
+app.get("/usuarios/:id", (req, res) => {
+    let id = req.params.id;
+    getUsuariosById(id).then(usuario => {
+        res.status(200).json({code: 200, data: usuario})
+    }).catch(error => {
+        console.log(error)
+        res.status(500).json({code: 500, error: "Error al cargar los usuarios."})
+    })
+})
+
+//rutas POR USUARIO
+app.post("/usuarios", (req, res) => {
+     let {nombre, email, password, imagen} = req.body;
+     addUsuario(nombre, email, password, imagen).then(usuario => {
+        res.status(201).json({code: 201, data: usuario})
+    }).catch(error => {
+        console.log(error)
+        res.status(500).json({code: 500, error: "Error al agregar un usuario."})
+    })
+})
+
+//rutas PUT USUARIO
+app.put("/usuarios", (req, res) => {
+    let {nombre, email, password, imagen, id} = req.body;
+    updateUsuario(nombre, email, password, imagen, id).then(usuario => {
+       res.status(201).json({code: 201, data: usuario})
+   }).catch(error => {
+       console.log(error)
+       res.status(500).json({code: 500, error: "Error al actualizar el usuario. " + nombre})
+   })
+})
+
+//RUTA DELETE USUARIO
+app.delete("/usuarios/:id", (req, res) => {
+    let id = req.params.id;
+    deleteUsuariosById(id).then(usuario => {
+        if(usuario.length == 0) return res.status(400).json({code: 400, error: "Ha intentado eliminar un usuario con un id desconocido"})
+        res.status(200).json({code: 200, data: usuario})
+    }).catch(error => {
+        console.log(error)
+        res.status(500).json({code: 500, error: "Error al eliminar el usuario con id: " + id})
+    })
+})
+
+
+
+
+
--- src/database/consultas.js
+++ src/database/consultas.js
@@ -11,3 +11,35 @@ const pool = new Pool({
   idleTimeoutMillis: 5000,
   connectionTimeoutMillis: 2000,
 })
+
+
+export const getUsuarios = async () => {
+    let usuarios = await pool.query("select id, nombre, email, imagen from usuarios");
+    return usuarios.rows;
+}
+
+export const getUsuariosById = async (id) => {
+    let usuario = await pool.query("select nombre, email, imagen from usuarios where id =$1", [id]);
+    return usuario.rows;
+}
+
+export const addUsuario= async (nombre, email, password, imagen) => {
+    let query =`
+        INSERT INTO usuarios(nombre, email, password, imagen)
+        values($1, $2, $3, $4) RETURNING *`    
+    let usuario = await pool.query(query, [nombre, email, password, imagen]);
+    return usuario.rows;
+}
+
+export const updateUsuario= async (nombre, email, password, imagen, id) => {
+    let query =`
+        UPDATE USUARIOS SET nombre = $1, email = $2, password = $3, imagen = $4 where id = $5 RETURNING id, nombre, email, imagen
+    `    
+    let usuario = await pool.query(query, [nombre, email, password, imagen, id]);
+    return usuario.rows;
+}
+
+export const deleteUsuariosById = async (id) => {
+    let usuario = await pool.query("DELETE FROM usuarios WHERE id =$1 RETURNING id, nombre, email, imagen ", [id]);
+    return usuario.rows;
+}
